e543ab2bb890b67c47a1274a73adbbfd8dc6178a
Egii <regidesoftian@gmail.com>
2022-07-20 16:22:23 +0000

    Add extras for selene-user 11 RP1A.200720.011 V12.5.19.0.RKUMIXM release-keys
    Signed-off-by: Egii <regidesoftian@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null >> system/system/lib64/librelight_only_extraphoto.so
+rm -f system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null
+cat system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null >> system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk
+rm -f system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/data-app/Photos/Photos.apk.* 2>/dev/null >> product/data-app/Photos/Photos.apk
+rm -f product/data-app/Photos/Photos.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk.* 2>/dev/null >> cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk
+rm -f cust/app/customized/partner-com.ss.android.ugc.trill_101/partner-com.ss.android.ugc.trill_101.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk
+rm -f cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.zing.zalo_172/partner-com.zing.zalo_172.apk.* 2>/dev/null >> cust/app/customized/partner-com.zing.zalo_172/partner-com.zing.zalo_172.apk
+rm -f cust/app/customized/partner-com.zing.zalo_172/partner-com.zing.zalo_172.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.bulldog_112/partner-com.kwai.bulldog_112.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.bulldog_112/partner-com.kwai.bulldog_112.apk
+rm -f cust/app/customized/partner-com.kwai.bulldog_112/partner-com.kwai.bulldog_112.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null >> cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk
+rm -f cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk.* 2>/dev/null >> cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk
+rm -f cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null >> cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk
+rm -f cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null >> cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk
+rm -f cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk.* 2>/dev/null >> cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk
+rm -f cust/app/customized/partner-com.booking_85/partner-com.booking_85.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk.* 2>/dev/null >> cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk
+rm -f cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk
+rm -f cust/app/customized/partner-com.kwai.video_33/partner-com.kwai.video_33.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null >> cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk
+rm -f cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk.* 2>/dev/null >> cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk
+rm -f cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null >> cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk
+rm -f cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null
